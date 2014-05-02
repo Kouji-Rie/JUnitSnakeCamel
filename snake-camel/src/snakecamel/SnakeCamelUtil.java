0452892455b78bfd91abc9bc4829d0797a7070b8
Kouji-Rie <g12921kr+git@gm.tsuda.ac.jp>
2014-05-02 17:54:13 +0900

    SnakeCamelUtil.javaの修正 テストの作成

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -30,13 +30,15 @@ public class SnakeCamelUtil {
 	}
 	
 	static String capitalize(String s) {
+		if(s.length()<=0) return s;
 		char first = s.charAt(0);
 		char upperFirst = Character.toUpperCase(first);
 		String rest = s.substring(1);
-		return upperFirst + rest;
+		return upperFirst + rest;	
 	}
 
 	static String uncapitalize(String s) {
+		if(s.length()<=0) return s;
 		char first = s.charAt(0);
 		char lowerFirst = Character.toLowerCase(first);
 		String rest = s.substring(1);
